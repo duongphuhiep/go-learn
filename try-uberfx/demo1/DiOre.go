9ea93389dd4c59e18155cdf7336e3524f520d0a2
Firas Darwish <f.darwish@diwink.com>
2024-10-28 15:40:06 +0300

    fixed ore issue with nested resolving #2

--- try-uberfx/demo1/DiOre.go
+++ try-uberfx/demo1/DiOre.go
@@ -6,89 +6,89 @@ import (
 	"github.com/firasdarwish/ore"
 )
 
-func (this *A) New(ctx context.Context) *A {
+func (this *A) New(ctx context.Context) (*A, context.Context) {
 	b, ctx := ore.Get[*B](ctx)
-	c, _ := ore.Get[*C](ctx)
-	return NewA(b, c)
+	c, ctx := ore.Get[*C](ctx)
+	return NewA(b, c), ctx
 }
-func (this *B) New(ctx context.Context) *B {
+func (this *B) New(ctx context.Context) (*B, context.Context) {
 	d, ctx := ore.Get[*D](ctx)
-	e, _ := ore.Get[*E](ctx)
-	return NewB(d, e)
+	e, ctx := ore.Get[*E](ctx)
+	return NewB(d, e), ctx
 }
-func (this *C) New(ctx context.Context) *C {
-	return NewC()
+func (this *C) New(ctx context.Context) (*C, context.Context) {
+	return NewC(), ctx
 }
-func (this *D) New(ctx context.Context) *D {
+func (this *D) New(ctx context.Context) (*D, context.Context) {
 	f, ctx := ore.Get[*F](ctx)
-	h, _ := ore.Get[*H](ctx)
-	return NewD(f, h)
+	h, ctx := ore.Get[*H](ctx)
+	return NewD(f, h), ctx
 }
-func (this *E) New(ctx context.Context) *E {
-	gs, _ := ore.GetList[G](ctx)
-	return NewE(gs)
+func (this *E) New(ctx context.Context) (*E, context.Context) {
+	gs, ctx := ore.GetList[G](ctx)
+	return NewE(gs), ctx
 }
-func (this *F) New(ctx context.Context) *F {
-	return NewF()
+func (this *F) New(ctx context.Context) (*F, context.Context) {
+	return NewF(), ctx
 }
-func (this *Ga) New(ctx context.Context) *Ga {
-	return NewGa()
+func (this *Ga) New(ctx context.Context) (*Ga, context.Context) {
+	return NewGa(), ctx
 }
-func (this *Gb) New(ctx context.Context) G {
-	return NewGb()
+func (this *Gb) New(ctx context.Context) (G, context.Context) {
+	return NewGb(), ctx
 }
-func (this *Gc) New(ctx context.Context) G {
-	return NewGc()
+func (this *Gc) New(ctx context.Context) (G, context.Context) {
+	return NewGc(), ctx
 }
-func (this *DGa) New(ctx context.Context) G {
-	ga, _ := ore.Get[*Ga](ctx)
-	return NewDGa(ga)
+func (this *DGa) New(ctx context.Context) (G, context.Context) {
+	ga, ctx := ore.Get[*Ga](ctx)
+	return NewDGa(ga), ctx
 }
-func (this *H) New(ctx context.Context) *H {
-	return NewH()
+func (this *H) New(ctx context.Context) (*H, context.Context) {
+	return NewH(), ctx
 }
 
 func RegisterDependenciesToOre_UseFunc() {
-	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) *A {
+	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) (*A, context.Context) {
 		b, ctx := ore.Get[*B](ctx)
-		c, _ := ore.Get[*C](ctx)
-		return NewA(b, c)
+		c, ctx := ore.Get[*C](ctx)
+		return NewA(b, c), ctx
 	})
-	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) *B {
+	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) (*B, context.Context) {
 		d, ctx := ore.Get[*D](ctx)
-		e, _ := ore.Get[*E](ctx)
-		return NewB(d, e)
+		e, ctx := ore.Get[*E](ctx)
+		return NewB(d, e), ctx
 	})
-	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) *C {
-		return NewC()
+	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) (*C, context.Context) {
+		return NewC(), ctx
 	})
-	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) *D {
+	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) (*D, context.Context) {
 		f, ctx := ore.Get[*F](ctx)
-		h, _ := ore.Get[*H](ctx)
-		return NewD(f, h)
+		h, ctx := ore.Get[*H](ctx)
+		return NewD(f, h), ctx
 	})
-	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) *E {
-		gs, _ := ore.GetList[G](ctx)
-		return NewE(gs)
+	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) (*E, context.Context) {
+		gs, ctx := ore.GetList[G](ctx)
+		return NewE(gs), ctx
 	})
-	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) *F {
-		return NewF()
+	ore.RegisterLazyFunc(ore.Transient, func(ctx context.Context) (*F, context.Context) {
+		return NewF(), ctx
 	})
-	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) *Ga {
-		return NewGa()
+	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) (*Ga, context.Context) {
+		return NewGa(), ctx
 	})
-	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) G {
-		return NewGb()
+	ore.RegisterLazyFunc(ore.Scoped, func(ctx context.Context) (G, context.Context) {
+		return NewGb(), ctx
 	})
-	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) G {
-		return NewGc()
+	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) (G, context.Context) {
+		return NewGc(), ctx
 	})
-	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) G {
-		ga, _ := ore.Get[*Ga](ctx)
-		return NewDGa(ga)
+	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) (G, context.Context) {
+		ga, ctx := ore.Get[*Ga](ctx)
+		return NewDGa(ga), ctx
 	})
-	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) *H {
-		return NewH()
+	ore.RegisterLazyFunc(ore.Singleton, func(ctx context.Context) (*H, context.Context) {
+		return NewH(), ctx
 	})
 }
 
